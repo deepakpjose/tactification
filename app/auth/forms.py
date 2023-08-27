4fe749f807ed2a529fec81a5b12f1b4b27029535
Deepak <deepakpjose@gmail.com>
2023-08-27 16:26:03 +0530

    edit posts

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,4 @@
+import logging
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired
 from wtforms import (
@@ -11,10 +12,10 @@ from wtforms import (
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError, validators
 
-
+#Ensure the fields name are same as in the fields in the Models for Post.
 class PosterCreateForm(FlaskForm):
     header = StringField("Header", [validators.Length(min=1, max=255)])
-    desc = TextAreaField("Description", [validators.Length(min=1, max=500)])
+    desc = TextAreaField("Image Caption", [validators.Length(min=1, max=500)])
     body = TextAreaField(
         "Body", [validators.Length(min=1)], render_kw={"rows": 10, "cols": 81}
     )
@@ -22,6 +23,27 @@ class PosterCreateForm(FlaskForm):
     poster = FileField("Image File", validators=[FileRequired()])
     submit = SubmitField("Submit")
 
+class PosterEditForm(FlaskForm):
+    header = StringField("Header", [validators.Length(min=1, max=255)])
+    description = TextAreaField("Image Caption", [validators.Length(min=1, max=500)])
+    body = TextAreaField(
+        "Body", [validators.Length(min=1)], render_kw={"rows": 10, "cols": 81}
+    )
+    tags = StringField("Tags", [validators.Length(min=1, max=255)])
+    poster = FileField("Image File")
+    submit = SubmitField("Submit")
+
+    def show(self):
+        header_data = 'header: {data}'
+        logging.info(header_data.format(data=self.header.data)) 
+        description_data = 'caption: {data}'
+        logging.info(description_data.format(data=self.description.data))
+        tags_data = 'tags: {data}'
+        logging.info(tags_data.format(data=self.tags.data))
+        if bool(self.poster.data):
+            poster_fields = 'poster: {filename}'
+            logging.info(poster_fields.format(filename=self.poster.data.filename))
+        return
 
 class LoginForm(FlaskForm):
     email = StringField(
